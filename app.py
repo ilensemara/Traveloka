e1cf1e3bebd36e17292d8cd1565f70cdd24d40f8
Shela Ilen <ilensemara@gmail.com>
2026-07-20 07:42:07 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+
+st.title("Aplikasi Dataku")
+st.write("Halo, ini aplikasi Streamlit dari Colab!")
+
+# Tulis sisa kode Python/Streamlit Anda di sini...
